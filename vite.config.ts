f36c7d5eb8318f3f7c7a37eef876b4734c1fffda
Sujith Lakmal <lakmalsujith25@gmail.com>
2026-05-31 05:24:55 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // <-- මේක තියෙනවාද බලන්න
 
+// https://vitejs.dev
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // <-- මේකත් මෙතන තියෙන්න ඕනේ
-  ],
-  base: "/GMC-Sri-Lanka/",
+  plugins: [react()],
+  base: "/GMC-Sri-Lanka/", // මේ පේළිය අනිවාර්යයෙන්ම එකතු කරන්න
 })
