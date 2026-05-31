7a2fe7fa44cf2863a2b3518dfdc156b7ac4ba8d3
Lakmal2078 <lakmalsujith25@gmail.com>
2026-05-31 05:55:36 +0530

    Fix config for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev
 export default defineConfig({
   plugins: [react()],
-  base: "/GMC-Sri-Lanka/", // මේ පේළිය අනිවාර්යයෙන්ම එකතු කරන්න
+  base: "/GMC-Sri-Lanka/", 
+  css: {
+    transformer: 'postcss' // CSS warning එක නැති කරගන්න මේක එකතු කරන්න
+  }
 })
+
