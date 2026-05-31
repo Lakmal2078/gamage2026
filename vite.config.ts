86be6d6e4b0e8d5f8d2061b8e519a16c65f3c95e
Lakmal2078 <lakmalsujith25@gmail.com>
2026-05-31 12:10:20 +0530

    linked index.css in index.html

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/gamage2026/', // 👈 මේ පේළිය අනිවාර්යයෙන්ම එකතු කරන්න!
+  plugins: [
+    react(),
+    tailwindcss(), // 👈 Tailwind Plugin එක මෙතැන තිබිය යුතුමයි!
+  ],
+  base: '/gamage2026/', // 👈 මුලට සහ අගට / ලකුණ අනිවාර්යයි!
 })
-
