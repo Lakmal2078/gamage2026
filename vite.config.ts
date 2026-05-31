26e19795bc1c2f52397b0be509021a455cadf0b2
Lakmal2078 <lakmalsujith25@gmail.com>
2026-05-31 06:10:40 +0530

    Update vite config css transformer

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev
 export default defineConfig({
   plugins: [react()],
   base: "/GMC-Sri-Lanka/", 
   css: {
-    transformer: 'postcss' // මේ කොටස අනිවාර්යයෙන්ම එකතු කරන්න
+    transformer: 'postcss' // මේ කොටස අනිවාර්යයෙන්ම තිබිය යුතුයි
   }
 })
 
